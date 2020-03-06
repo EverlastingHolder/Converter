784ca1968a88a71cfe5e17fcd47e00f7097fac37
asd <sad>
2020-03-06 12:35:22 +0700

    Initial Commit

---
+++ Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Converter
+//
+//  Created by admin on 06.03.2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
